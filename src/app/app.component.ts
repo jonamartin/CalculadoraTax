6461d8504f1b3586c58041717a02e9cc39169379
Jonathan Martin <60260218+jonamartin@users.noreply.github.com>
2020-10-12 01:43:59 -0300

    Funcionalidad y maquetado sin diseño

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tax-calculator';
+  
+  public focusOnTax: boolean;
+  public arrayForTax: Array<any> = [];
+  public numberForTax: number;
+  public arrayForPrice: Array<any> = [];
+  public numberForPrice: number;
+  public resultado: number;
+
+  focusTax(boolean)
+  {
+    this.focusOnTax = boolean;
+  }
+  write(x)
+  {
+    if(this.focusOnTax)
+    {
+      this.arrayForTax.push(x);
+      this.numberForTax = parseFloat(this.arrayForTax.join(''));
+      this.calcular();
+    }
+    else
+    {
+      this.arrayForPrice.push(x);
+      this.numberForPrice = parseFloat(this.arrayForPrice.join(''));
+      this.calcular();
+    }
+  }
+  del()
+  {
+    if(this.focusOnTax)
+    {
+      this.arrayForTax.pop();
+      this.numberForTax = parseFloat(this.arrayForTax.join(''));
+      if(this.numberForTax === NaN) {this.numberForTax = 0}
+      this.calcular();
+    }
+    else
+    {
+      this.arrayForPrice.pop();
+      this.numberForPrice = parseFloat(this.arrayForPrice.join(''));
+      if(this.numberForTax === NaN) {this.numberForTax = 0}
+      this.calcular();
+    }
+
+  }
+
+  calcular()
+  {
+    this.resultado = (this.numberForPrice*(this.numberForTax+100))/100 
+  }
 }
